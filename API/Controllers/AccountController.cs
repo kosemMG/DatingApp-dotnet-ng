2c25a1a0131469746b4bee986dca85b737c31660
Moshe Gelberg <glbrgm@gmail.com>
2021-02-26 08:41:51 +0200

    Identity and role management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,12 +1,11 @@
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,13 +13,15 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
 
-        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenService tokenService, IMapper mapper)
         {
-            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
             _tokenService = tokenService;
             _mapper = mapper;
         }
@@ -32,19 +33,20 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
-            await _context.Users.AddAsync(user);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var roleResult = await _userManager.AddToRoleAsync(user, "Member");
+            
+            if (!roleResult.Succeeded) return BadRequest(roleResult.Errors);
 
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -53,18 +55,16 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
-                .Include(user => user.Photos)
-                .SingleOrDefaultAsync(appUser => appUser.UserName == loginDto.Username);
+            var user = await _userManager.Users
+                .Include(appUser => appUser.Photos)
+                .SingleOrDefaultAsync(appUser => appUser.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
-            var hmac = new HMACSHA512(user.PasswordSalt);
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            const bool lockoutOnFailure = false;
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, lockoutOnFailure);
 
-            for (int i = 0; i < computedHash.Length; i++)
-                if (computedHash[i] != user.PasswordHash[i])
-                    return Unauthorized("Invalid password");
+            if (!result.Succeeded) return Unauthorized();
 
             var mainPhotoUrl = user.Photos.FirstOrDefault(photo => photo.IsMain)?.Url;
 
@@ -72,7 +72,7 @@ namespace API.Controllers
             {
                 Username = user.UserName,
                 KnownAs = user.KnownAs,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = mainPhotoUrl,
                 Gender = user.Gender
             };
@@ -80,7 +80,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(user => user.UserName == username.ToLower());
         }
     }
 }
--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,46 +1,60 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+  public class DataContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole,
+    IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+  {
+    public DataContext(DbContextOptions options) : base(options)
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
-
-        public DbSet<AppUser> Users { get; set; }
-        public DbSet<UserLike> Likes { get; set; }
-        public DbSet<Message> Messages { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<UserLike>()
-                .HasKey(like => new {like.SourceUserId, like.LikedUserId});
-
-            modelBuilder.Entity<UserLike>()
-                .HasOne(like => like.SourceUser)
-                .WithMany(like => like.LikedUsers)
-                .HasForeignKey(like => like.SourceUserId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<UserLike>()
-                .HasOne(like => like.LikedUser)
-                .WithMany(like => like.LikedByUsers)
-                .HasForeignKey(like => like.LikedUserId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Message>()
-                .HasOne(message => message.Recipient)
-                .WithMany(user => user.MessagesReceived)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Message>()
-                .HasOne(message => message.Sender)
-                .WithMany(user => user.MessagesSent)
-                .OnDelete(DeleteBehavior.Restrict);
-        }
     }
+
+    public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      modelBuilder.Entity<AppUser>()
+        .HasMany(user => user.UserRoles)
+        .WithOne(userRole => userRole.User)
+        .HasForeignKey(userRole => userRole.UserId)
+        .IsRequired();
+
+      modelBuilder.Entity<AppRole>()
+        .HasMany(role => role.UserRoles)
+        .WithOne(userRole => userRole.Role)
+        .HasForeignKey(userRole => userRole.RoleId)
+        .IsRequired();
+
+      modelBuilder.Entity<UserLike>()
+        .HasKey(like => new {like.SourceUserId, like.LikedUserId});
+
+      modelBuilder.Entity<UserLike>()
+        .HasOne(like => like.SourceUser)
+        .WithMany(like => like.LikedUsers)
+        .HasForeignKey(like => like.SourceUserId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+      modelBuilder.Entity<UserLike>()
+        .HasOne(like => like.LikedUser)
+        .WithMany(like => like.LikedByUsers)
+        .HasForeignKey(like => like.LikedUserId)
+        .OnDelete(DeleteBehavior.Cascade);
+
+      modelBuilder.Entity<Message>()
+        .HasOne(message => message.Recipient)
+        .WithMany(user => user.MessagesReceived)
+        .OnDelete(DeleteBehavior.Restrict);
+
+      modelBuilder.Entity<Message>()
+        .HasOne(message => message.Sender)
+        .WithMany(user => user.MessagesSent)
+        .OnDelete(DeleteBehavior.Restrict);
+    }
+  }
 }
