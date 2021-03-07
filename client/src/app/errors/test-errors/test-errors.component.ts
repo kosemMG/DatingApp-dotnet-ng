86ea460480713ae56551fda283dd919f443a3960
Moshe Gelberg <glbrgm@gmail.com>
2021-03-07 08:34:06 +0200

    Pre-switch to Postgres

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -9,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestErrorsComponent implements OnInit {
   public validationErrors: string[] = [];
 
-  private baseUrl = 'http://localhost:5000/api/';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {
   }
